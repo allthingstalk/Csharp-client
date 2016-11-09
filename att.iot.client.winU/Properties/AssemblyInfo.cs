d613364cec4c24903ca793a3e0aefee91c570558
Jan Bogaerts <jb@allthingstalk.com>
2016-11-09 16:24:30 +0100

    update packages to latest versions, set lib version, update nuget package definition

--- att.iot.client.winU/Properties/AssemblyInfo.cs
+++ att.iot.client.winU/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AllThingsTalk")]
 [assembly: AssemblyProduct("att.iot.client")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 [assembly: ComVisible(false)]
